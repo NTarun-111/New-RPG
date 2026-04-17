d683339b6cf7e868444e3048874add501cf7e273
Tarun N <tarunprinix@gmail.com>
2026-04-17 14:07:18 +0530

    Cooldown Timer

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+    }
+    
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + " took some Damage!");
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
+    }
+
+    private void TurnWhite()
+    {
+        sr.color = Color.white;
     }
 }
